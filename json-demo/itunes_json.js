087ab86560a6679fb670b1734f1b79c3b9195aaf
Daniel Griffin <dgriffinpv@gmail.com>
2023-11-16 10:50:23 -0700

    initial commit: finishes json excercise

---
+++ json-demo/itunes_json.js
@@ -0,0 +1,9 @@
+import mountainSongs from './mountain.json' assert { type: 'json'}
+
+// console.log(Object.keys(mountainSongs))
+
+// console.log(mountainSongs.results[0].trackId)
+
+console.log(mountainSongs.results[1])
+
+console.log(Object.keys(mountainSongs.results[0]))
